b88868950385c58e73fa6d31ad9500dd8302e23e
t.phil <thomas@tphil.nl>
2020-08-14 14:05:22 +0200

    bugfix

--- scripts/covidwatchnl.py
+++ scripts/covidwatchnl.py
@@ -37,23 +37,23 @@ df_rivmnums = df_transformed.copy()
 # -------------------------------------------------------------------------
 # -------------------------------------------------------------------------
 
-df_tests = pd.read_csv('https://github.com/J535D165/CoronaWatchNL/raw/master/data/rivm_NL_covid19_tests.csv')
-
-transformed_data = {}
-for idx, row in df_tests.iterrows():
-    if row['Datum'] not in transformed_data:
-        transformed_data[row['Datum']] = {}
-    transformed_data[row['Datum']][f'Labs'] = row['Labs']
-    transformed_data[row['Datum']][f'{row["Type"]}'] = row['Aantal']
-
-df_transformed = pd.DataFrame.from_dict(transformed_data, orient='index')
-df_transformed.rename(
-    columns={'Labs': 'labs_participating', 'Totaal': 'tests_cumulative', 'Positief': 'positive_tests_cumulative'},
-    inplace=True)
-df_transformed['tests'] = df_transformed['tests_cumulative'].diff().fillna(0).astype(int)
-df_transformed['positive_tests'] = df_transformed['positive_tests_cumulative'].diff().fillna(0).astype(int)
-
-df_out = df_rivmnums.join(df_transformed)
+#  df_tests = pd.read_csv('https://github.com/J535D165/CoronaWatchNL/raw/master/data/rivm_NL_covid19_tests.csv')
+#  
+#  transformed_data = {}
+#  for idx, row in df_tests.iterrows():
+#      if row['Datum'] not in transformed_data:
+#          transformed_data[row['Datum']] = {}
+#      transformed_data[row['Datum']][f'Labs'] = row['Labs']
+#      transformed_data[row['Datum']][f'{row["Type"]}'] = row['Aantal']
+#  
+#  df_transformed = pd.DataFrame.from_dict(transformed_data, orient='index')
+#  df_transformed.rename(
+#      columns={'Labs': 'labs_participating', 'Totaal': 'tests_cumulative', 'Positief': 'positive_tests_cumulative'},
+#      inplace=True)
+#  df_transformed['tests'] = df_transformed['tests_cumulative'].diff().fillna(0).astype(int)
+#  df_transformed['positive_tests'] = df_transformed['positive_tests_cumulative'].diff().fillna(0).astype(int)
+#  
+#  df_out = df_rivmnums.join(df_transformed)
 
 
 # -------------------------------------------------------------------------
