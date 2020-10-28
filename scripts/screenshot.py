b3323bfeb212a29f595ccc85ed52d13a2035a80c
t.phil <thomas@tphil.nl>
2020-10-28 12:07:23 +0100

    test multiprocessing

--- scripts/screenshot.py
+++ scripts/screenshot.py
@@ -18,6 +18,8 @@ print(last_update_str)
 with open('data/charts.yml', 'r') as fh:
     pdata = yaml.safe_load(fh)
 
+cmds = []
+
 for page, data in pdata['pages'].items():
     url = "https://covid-analytics.nl/" + data['file']
     for chart in data['charts']:
@@ -26,4 +28,15 @@ for page, data in pdata['pages'].items():
         workdir = Path('./screenshots')
         dockercmd = f"docker run -it -w /usr/workspace -v {workdir.absolute()}:/usr/workspace joyzoursky/python-chromedriver:3.8-selenium bash -c \"python screencap.py '{chart_url}' {output}\""
         print(dockercmd)
-        os.system(dockercmd)
+        cmds.append(dockercmd)
+
+
+from multiprocessing import Pool
+
+def runcmd(cmd):
+    os.system(cmd)
+    
+
+with Pool(processes=8) as pool:
+    result = pool.map(runcmd, dockercmd)
+
