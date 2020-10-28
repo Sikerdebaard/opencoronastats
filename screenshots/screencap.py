e5c42479b60937693bb3f5683be8111b3cbabe37
t.phil <thomas@tphil.nl>
2020-10-28 11:16:58 +0100

    automatic screenshots

---
+++ screenshots/screencap.py
@@ -0,0 +1,51 @@
+import os
+
+from optparse import OptionParser
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+CHROME_PATH = '/usr/bin/google-chrome'
+CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
+WINDOW_SIZE = "2560,1440"
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--ignore-certificate-errors")
+chrome_options.add_argument("--verbose")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.binary_location = CHROME_PATH
+
+def make_screenshot(url, output):
+
+    driver = webdriver.Chrome(
+        executable_path=CHROMEDRIVER_PATH,
+        chrome_options=chrome_options
+    )
+    driver.get(url)
+
+    driver.save_screenshot("/tmp/tmp.png")
+
+    el = driver.find_element_by_id(url.split('#')[1]);
+
+    print(el.location)
+    print(el.size)
+
+    el.screenshot(output)
+
+    driver.close()
+
+if __name__ == '__main__':
+    usage = "usage: %prog [options] <url> <output>"
+    parser = OptionParser(usage=usage)
+
+    (options, args) = parser.parse_args()
+
+    if len(args) < 2:
+        parser.error("please specify a URL and an output")
+
+    make_screenshot(args[0], args[1])
+
+
---
+++ scripts/screenshot.py
@@ -0,0 +1,29 @@
+import yaml
+from jinja2 import Environment, FileSystemLoader, select_autoescape, Markup
+import markdown
+from datetime import datetime
+from pytz import timezone
+from pathlib import Path
+import json
+import time
+
+output_path = Path('./screenshots')
+
+
+tz = timezone('Europe/Amsterdam')
+last_update = datetime.now().astimezone(tz)
+last_update_str = last_update.strftime("%Y%m%d-%H%M")
+
+print(last_update_str)
+
+with open('data/charts.yml', 'r') as fh:
+    pdata = yaml.safe_load(fh)
+
+for page, data in pdata['pages'].items():
+    url = "https://covid-analytics.nl/" + data['file']
+    for chart in data['charts']:
+        chart_url = url + '#' + chart['name']
+        output = f"{last_update_str}-{chart['name']}.png"
+        workdir = Path('./screenshots')
+        dockercmd = f"docker run -it -w /usr/workspace -v {workdir.absolute()}:/usr/workspace joyzoursky/python-chromedriver:3.8-selenium bash -c \"python screencap.py '{chart_url}' {output}\""
+	os.system(dockercmd)
