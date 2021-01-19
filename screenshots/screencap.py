94d60503473938444af1b238cd6814999717a4a1
t.phil <thomas@tphil.nl>
2021-01-19 20:11:55 +0100

    test

--- screenshots/screencap.py
+++ screenshots/screencap.py
@@ -22,8 +22,47 @@ chrome_options.add_argument("--disable-web-security")
 chrome_options.add_argument("--allow-file-access-from-files")
 chrome_options.binary_location = CHROME_PATH
 
+
+fetchstub = """
+if (/^file:\/\/\//.test(location.href)) {
+    let path = './';
+    let orig = fetch;
+    window.fetch = (resource) => ((/^[^/:]*:/.test(resource)) ?
+        orig(resource) :
+        new Promise(function(resolve, reject) {
+            let request = new XMLHttpRequest();
+
+            let fail = (error) => {reject(error)};
+            ['error', 'abort'].forEach((event) => { request.addEventListener(event, fail); });
+
+            let pull = (expected) => (new Promise((resolve, reject) => {
+                if (
+                    request.responseType == expected ||
+                    (expected == 'text' && !request.responseType)
+                )
+                    resolve(request.response);
+                else
+                    reject(request.responseType);
+            }));
+
+            request.addEventListener('load', () => (resolve({
+                arrayBuffer : () => (pull('arraybuffer')),
+                blob        : () => (pull('blob')),
+                text        : () => (pull('text')),
+                json        : () => (pull('json'))
+            })));
+            request.open('GET', resource.replace(/^\//, path));
+            request.send();
+        })
+    );
+}
+"""
+
+
 def make_screenshot(driver, url, output):
     driver.get(url)
+
+    driver.execute_script(fetchstub)
 
     retval = ""
     while retval.lower().strip() != 'complete':
