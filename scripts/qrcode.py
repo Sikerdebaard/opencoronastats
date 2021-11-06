8efbba48f5565fcaa6da1de2dfea0543bd5e94fa
t.phil <thomas@tphil.nl>
2021-11-06 15:57:50 +0100

    Renamed QR blocklist csv

--- scripts/qrcode.py
+++ scripts/qrcode.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-df_denylist = pd.read_csv('https://raw.githubusercontent.com/Sikerdebaard/coronacheck-denylist/main/data/denylist.csv', index_col=0)
+df_denylist = pd.read_csv('https://raw.githubusercontent.com/Sikerdebaard/coronacheck-denylist/main/data/latest-denylist.csv', index_col=0)
 df_denylist.to_csv('html/qr-denylist.csv')
