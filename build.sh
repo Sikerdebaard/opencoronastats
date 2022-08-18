2b41ab3904eaa34a9f428f4744ec29679585f6e2
T Phil <thomas@tphil.nl>
2022-08-18 10:53:08 +0200

    Disabled broken scripts

--- build.sh
+++ build.sh
@@ -31,11 +31,11 @@
 `which time` -ao logs/log-pip pip install -r ./requirements.txt
 
 `which time` -ao logs/log-qrcode python scripts/qrcode.py &
-`which time` -ao logs/log-qrcode python scripts/ecdc_vaccine.py &
+#`which time` -ao logs/log-qrcode python scripts/ecdc_vaccine.py &
 `which time` -ao logs/log-generate_data python scripts/generate_data.py &
 `which time` -ao logs/log-hospitalized python scripts/hospitalized.py &
 `which time` -ao logs/log-covidwatchnl python scripts/covidwatchnl.py &
-`which time` -ao logs/log-mobility python scripts/mobility.py &
+# `which time` -ao logs/log-mobility python scripts/mobility.py &   # mobility is broken
 `which time` -ao logs/log-births python scripts/births.py &
 `which time` -ao logs/log-case-counts python scripts/case-counts.py &
 `which time` -ao logs/log-euromomo python scripts/euromomo.py &
@@ -43,11 +43,11 @@
 `which time` -ao logs/log-ggd-tests-performed python scripts/ggd-tests-performed.py &
 wait
 
-`which time` -ao logs/log-healthcare-workers python scripts/healthcare-workers.py &
+# `which time` -ao logs/log-healthcare-workers python scripts/healthcare-workers.py &  # disabled
 `which time` -ao logs/log-agedistributions python scripts/agedistributions.py &
 #python scripts/outbreakmonitor.py &
 #python scripts/excessmortality.py &
-`which time` -ao logs/log-mortality-displacement python scripts/mortality-displacement.py &
+#`which time` -ao logs/log-mortality-displacement python scripts/mortality-displacement.py &  # mortality displacement broken
 #`which time` -ao logs/log-monkeypatch-lcps python scripts/monkeypatch-lcps.py &
 wait
 
